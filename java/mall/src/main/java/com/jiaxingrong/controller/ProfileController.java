4c7163c7945c1047dd17441ef8b5c5e7f4a7ee46
xingrong-jia <766718726@qq.com>
2019-12-23 19:10:13 +0800

    提交者：贾兴荣

--- java/mall/src/main/java/com/jiaxingrong/controller/ProfileController.java
+++ java/mall/src/main/java/com/jiaxingrong/controller/ProfileController.java
@@ -1,8 +1,7 @@
 package com.jiaxingrong.controller;
 
-import com.jiaxingrong.model.Admin;
+import com.jiaxingrong.model.AdminPassword;
 import com.jiaxingrong.model.BaseReqVo;
-import com.jiaxingrong.model.InfoData;
 import com.jiaxingrong.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,17 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("admin/auth")
-public class AuthController {
+@RequestMapping("admin/profile")
+public class ProfileController {
 
     @Autowired
     AdminService adminService;
 
-    @RequestMapping("login")
-    public BaseReqVo login(@RequestBody Admin admin) {
-        boolean b = adminService.login(admin);
+    @RequestMapping("password")
+    public BaseReqVo password(@RequestBody AdminPassword AdminPassword) {
+        int b = adminService.changeAdminPassword(AdminPassword);
         BaseReqVo<String> baseReqVo = new BaseReqVo<>();
-        if (b) {
+        if (b==1) {
             baseReqVo.setData("4b7d719e-53b7-4019-9677-6309b2445b45");
             baseReqVo.setErrno(0);
             baseReqVo.setErrmsg("成功");
@@ -32,13 +31,4 @@ public class AuthController {
         return baseReqVo;
     }
 
-    @RequestMapping("info")
-    public BaseReqVo info(String token) {
-        InfoData infoData = adminService.info(token);
-        BaseReqVo baseReqVo = new BaseReqVo();
-        baseReqVo.setData(infoData);
-        baseReqVo.setErrmsg("成功");
-        baseReqVo.setErrno(0);
-        return baseReqVo;
-    }
 }
--- java/mall/src/main/java/com/jiaxingrong/mapper/AdminMapper.java
+++ java/mall/src/main/java/com/jiaxingrong/mapper/AdminMapper.java
@@ -2,7 +2,9 @@ package com.jiaxingrong.mapper;
 
 import com.jiaxingrong.model.Admin;
 import com.jiaxingrong.model.AdminExample;
+
 import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 public interface AdminMapper {
@@ -19,6 +21,8 @@ public interface AdminMapper {
     List<Admin> selectByExample(AdminExample example);
 
     Admin selectByPrimaryKey(Integer id);
+
+    Integer selectIdByUsernameAndPassword(@Param("admin") Admin admin);
 
     int updateByExampleSelective(@Param("record") Admin record, @Param("example") AdminExample example);
 
