66793beb9aa2cdbfc605e72ff9ec6563d76a5886
sereneVincent <761596616@qq.com>
2019-12-26 16:01:07 +0800

    提交者:小淼

--- javaCode/mall/src/main/java/com/jiaxingrong/controller/admin/AuthController.java
+++ javaCode/mall/src/main/java/com/jiaxingrong/controller/admin/AuthController.java
@@ -4,7 +4,6 @@ import com.jiaxingrong.model.Admin;
 import com.jiaxingrong.model.BaseReqVo;
 import com.jiaxingrong.model.InfoData;
 import com.jiaxingrong.service.AdminService;
-import com.jiaxingrong.shiro.CustomToken;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.subject.Subject;
@@ -12,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -21,16 +22,16 @@ public class AuthController {
     @Autowired
     AdminService adminService;
 
-       @RequestMapping("admin/auth/login")
+       @RequestMapping("login")
 //    public BaseRespVo login(@RequestBody LoginBean loginBean)
-    public BaseRespVo login(@RequestBody Map map){
+    public BaseReqVo login(@RequestBody Map map){
         //没有使用shiro的情况下 ，返回任意的这个值就行了
         //errno = 0
-        BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
-        baseRespVo.setErrno(0);
-        baseRespVo.setData("abc");
-        baseRespVo.setErrmsg("成功");
-        return baseRespVo;
+        BaseReqVo<Object> baseReqVo = new BaseReqVo<>();
+        baseReqVo.setErrno(0);
+        baseReqVo.setData("abc");
+        baseReqVo.setErrmsg("成功");
+        return baseReqVo;
 
     }
 
@@ -52,7 +53,7 @@ public class AuthController {
         baseRespVo.setData(map);
         return baseRespVo;
     }*/
-    @RequestMapping("admin/auth/info")
+    @RequestMapping("info")
     public String info(){
         String resp = "{\"errno\":0,\"data\":{\"roles\":[\"超级管理员\"],\"name\":\"admin123\",\"perms\":[\"*\"],\"avatar\":\"https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif\"},\"errmsg\":\"成功\"}";
         return resp;
